47fe37dbc20cf84ed24103630ed3ecbbd9afadb0
Pedaprolu Geethika <109152655+Geethikapedaprolu12@users.noreply.github.com>
2024-05-17 23:28:59 +0530

    Create scriptt.js

---
+++ scriptt.js
@@ -0,0 +1,10 @@
+<script>
+  function displayMessage() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = 'Changing the message from "javascript to text"';
+  }
+
+  function showMessageAlert() {
+    alert('Please fill all the required data')
+  } 
+</script>
